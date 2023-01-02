71d5047f6c74733d1250d72a70490d9edca96191
umarluqman <umarluqman.78@gmail.com>
2023-01-02 13:31:52 +0800

    formatting

--- generics-with-keyof.ts
+++ generics-with-keyof.ts
@@ -13,28 +13,28 @@ const plucked = <DataType, KeyType extends keyof DataType>(
 };
 
 const dogs = [
-  { name: 'Raul', age: 2 },
-  { name: 'Tika', age: 5 },
+  { name: "Raul", age: 2 },
+  { name: "Tika", age: 5 },
 ];
 
-console.log(plucked(dogs, 'name'));
-console.log(plucked(dogs, 'age'));
+console.log(plucked(dogs, "name"));
+console.log(plucked(dogs, "age"));
 
 interface BaseEvent {
   time: number;
   user: string;
 }
 
-interface EventMap {
+interface CommerceEventMap {
   addToCart: BaseEvent & { quantity: number; productID: string };
   checkout: BaseEvent;
 }
 
-const sendEvent = <Name extends keyof EventMap>(
+const sendEvent = <Name extends keyof CommerceEventMap>(
   name: Name,
-  data: EventMap[Name]
+  data: CommerceEventMap[Name]
 ): void => {
   console.log([name, data]);
 };
 
-sendEvent('checkout', { time: 12, user: 'Ahmad' });
+sendEvent("checkout", { time: 12, user: "Ahmad" });
